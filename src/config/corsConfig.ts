b0b329e4cf10e29a2fb36817d31a922dbf6a1389
nitinthakurdev <radheradhe9870023@gmail.com>
2026-09-12 21:45:16 +0530

    now the changes verify

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -2,7 +2,7 @@ import type { CorsOptions } from "cors";
 import { config } from "./envConfig";
 
 
-export const corsOptions: CorsOptions = {
+export const corsOptions: CorsOptions = { 
   origin:config.CLIENT_URL,
   methods: ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
   credentials: true,
